4b709497f8308b43ac57ee6d1175898254a3d947
Carlos Sánchez Cazorla <cjsanchez@uloyola.es>
2018-10-05 12:18:15 +0200

    First commit

---
+++ Assets/velocity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class velocity : MonoBehaviour {
+
+	private Rigidbody rb; 
+	public Vector3 velocidadInicial = new Vector3(0,0,0);
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = velocidadInicial;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Velocity of " + transform.name + ": " + rb.velocity);
+	}
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Ejemplos de dinámica en Unity para la asignatura de Modelado de Sistemas Físicos por Computador del Grado de Informática y Tecnologías Virtuales de la Universidad Loyola Andalucía
